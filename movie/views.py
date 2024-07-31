7b1a351eb5cbcd845209e562445cc555dac0c126
SebasUr <sebasurberu1@gmail.com>
2024-07-31 16:13:34 -0500

    búsqueda de películas

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
 # Create your views here.
 
 def home(request):
     # return HttpResponse("<h1>Welcome to home page</h1>")
     # return render(request, "home.html")
-    return render(request, "home.html", {"name":"Sebastián Uribe"})
+    # return render(request, "home.html", {"name":"Sebastián Uribe"})
+
+    searchTerm = request.GET.get("searchMovie")
+    if searchTerm:
+        movies = Movie.objects.filter(title__contains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    
+    return render(request,"home.html", {"searchTerm":searchTerm, "movies": movies})
 
 def about(request):
     # return HttpResponse("<h1>Welcome to About page</h1>")
